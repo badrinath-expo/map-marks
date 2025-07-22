8e76272fee7170afcfce80fb7a846440e78517a0
Badrinath Pillalamarri <badrinathp.be21@gmail.com>
2025-07-22 17:34:11 +0000

    use this icon in the search, remove the 'G' icon that you're using

--- src/components/location-search.tsx
+++ src/components/location-search.tsx
@@ -51,8 +51,43 @@ export function LocationSearch({ onPlaceSelect }: { onPlaceSelect: (place: googl
       <div className="flex w-full items-center">
         <div className="relative flex-grow">
           <div className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground">
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48">
-                <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.222,0-9.519-3.355-11.297-7.962l-6.571,4.819C9.656,39.663,16.318,44,24,44z"></path><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.089,5.571l6.19,5.238C44.438,36.338,48,30.668,48,24C48,22.659,47.862,21.35,47.611,20.083z"></path>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                d="M12 2C8.13 2 5 5.13 5 9c0 4.25 7 13 7 13s7-8.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 010-5 2.5 2.5 0 010 5z"
+                fill="#4285F4"
+              />
+              <path
+                d="M12 2C8.13 2 5 5.13 5 9c0 4.25 7 13 7 13s7-8.75 7-13c0-3.87-3.13-7-7-7z"
+                fill="url(#paint0_linear_1_2)"
+              />
+              <path d="M12 11.5a2.5 2.5 0 000-5 2.5 2.5 0 000 5z" fill="#EA4335" />
+              <path
+                d="M12 2c-1.93 0-3.7.77-5.02 2.02l12.04 12.04C19.82 14.9 21 12.1 21 9c0-3.87-3.13-7-7-7z"
+                fill="#FBBC05"
+              />
+              <path
+                d="M12 22s7-8.75 7-13c0-1.5-.47-2.89-1.26-4.02L5.22 17.5C6.88 19.95 12 22 12 22z"
+                fill="#34A853"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_1_2"
+                  x1="12"
+                  y1="2"
+                  x2="12"
+                  y2="22"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="white" stopOpacity="0.4" />
+                  <stop offset="1" stopColor="white" stopOpacity="0" />
+                </linearGradient>
+              </defs>
             </svg>
           </div>
           <Input
